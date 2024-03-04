447c6dadbb8d4e57d9f08c203040178980c26e1d
Alireza Arvandi <alireza.arv.mail@gmail.com>
2024-03-03 21:19:41 -0400

    creating the files and the structure of the application

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import * as http from 'http';
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, TypeScript with Node.js!');
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
